8da91f19b3b03b8474ce2d48772c3e838e60df24
salty-max <max@jellycat.fr>
2022-11-02 18:28:34 +0100

    implements basic ast types

---
+++ src/ast.ts
@@ -0,0 +1,49 @@
+export type NodeType =
+  | 'Program'
+  | 'NumericLiteral'
+  | 'Identifier'
+  | 'StringLiteral'
+  | 'BooleanLiteral'
+  | 'UnaryExpr'
+  | 'PostfixExpr'
+  | 'BinaryExpr'
+  | 'FunctionDecl'
+
+export interface Stmt {
+  kind: NodeType
+}
+
+export interface Program extends Stmt {
+  kind: 'Program'
+  body: Array<Stmt>
+}
+
+// deno-lint-ignore no-empty-interface
+export interface Expr extends Stmt {}
+
+export interface BinaryExpr extends Expr {
+  kind: 'BinaryExpr'
+  operator: string
+  lhs: Expr
+  rhs: Expr
+}
+
+export interface Identifier extends Expr {
+  kind: 'Identifier'
+  symbol: string
+}
+
+export interface NumericLiteral extends Expr {
+  kind: 'NumericLiteral'
+  value: number
+}
+
+export interface StringLiteral extends Expr {
+  kind: 'StringLiteral'
+  value: string
+}
+
+export interface BooleanLiteral extends Expr {
+  kind: 'BooleanLiteral'
+  value: boolean
+}
